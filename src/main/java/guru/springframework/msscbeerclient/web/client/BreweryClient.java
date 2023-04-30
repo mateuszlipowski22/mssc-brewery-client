be95361972881d487b0b0a657f473d7aaeea7e43
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-04-30 21:29:27 +0200

    adding post method to restTemplate

--- src/main/java/guru/springframework/msscbeerclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbeerclient/web/client/BreweryClient.java
@@ -6,13 +6,14 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Component
-@ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
+@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
 public class BreweryClient {
 
-    private final String BEER_PATH_V1 = "/api/v1/beer/";
+    private final String BEER_PATH_V1 = "/api/v1/beer";
     private String apihost;
     private final RestTemplate restTemplate;
 
@@ -21,7 +22,11 @@ public class BreweryClient {
     }
 
     public BeerDto getBeerById(UUID uuid){
-        return restTemplate.getForObject(apihost+BEER_PATH_V1+uuid.toString(), BeerDto.class);
+        return restTemplate.getForObject(apihost+BEER_PATH_V1+"/"+uuid.toString(), BeerDto.class);
+    }
+
+    public URI saveNewBeer(BeerDto beerDto){
+        return restTemplate.postForLocation(apihost+BEER_PATH_V1, beerDto);
     }
 
     public void setApihost(String apihost) {
