e82549d97359729971620c055437d1d505ad84b9
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-05-01 14:39:08 +0200

    updating client to import org.apache.hc.client5.http.classic.HttpClient;

--- src/main/java/guru/springframework/msscbeerclient/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/guru/springframework/msscbeerclient/web/config/BlockingRestTemplateCustomizer.java
@@ -1,32 +1,44 @@
 package guru.springframework.msscbeerclient.web.config;
 
 
-//@Component
-//public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
-//
-//    public ClientHttpRequestFactory ClientHttpRequestFactory(){
-//        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-//        connectionManager.setMaxTotal(100);
-//        connectionManager.setDefaultMaxPerRoute(20);
-//
-//        RequestConfig requestConfig = RequestConfig
-//                .custom()
-//                .setConnectionRequestTimeout(3000)
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.DefaultConnectionKeepAliveStrategy;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.util.Timeout;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+
+    public ClientHttpRequestFactory ClientHttpRequestFactory(){
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(100);
+        connectionManager.setDefaultMaxPerRoute(20);
+
+        RequestConfig requestConfig = RequestConfig
+                .custom()
+                .setConnectionRequestTimeout(Timeout.ofMilliseconds(3000))
 //                .setSocketTimeout(3000)
-//                .build();
-//
-//        CloseableHttpClient httpClient = HttpClients
-//                .custom()
-//                .setConnectionManager(connectionManager)
-//                .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
-//                .setDefaultRequestConfig(requestConfig)
-//                .build();
-//
-//        return new HttpComponentsClientHttpRequestFactory(httpClient);
-//    }
-//
-//    @Override
-//    public void customize(RestTemplate restTemplate) {
-//        restTemplate.setRequestFactory(this.ClientHttpRequestFactory());
-//    }
-//}
+                .build();
+
+        HttpClient httpClient = HttpClients
+                .custom()
+                .setConnectionManager(connectionManager)
+                .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
+    }
+
+    @Override
+    public void customize(RestTemplate restTemplate) {
+        restTemplate.setRequestFactory(this.ClientHttpRequestFactory());
+    }
+}
